d9437b2935fdc8a82a2eebebabe86133fee0f39a
Phayao Boonon <Phayao@Phayaos-MacBook-Pro.local>
2016-12-25 17:15:03 +0700

    add functional of authenticate

--- start/app.js
+++ start/app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 var api = require('./routes/api');
-//var authenticate = require('./routes/authenticate');
+var authenticate = require('./routes/authenticate')(passport);
 
 var app = express();
 
@@ -17,13 +19,22 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(session({
+  secret: 'super duper secret'
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api', api);
-//app.use('/auth', authenticate);
+app.use('/auth', authenticate);
+
+var initPassport = require('./passport-init');
+initPassport(passport);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- start/passport-init.js
+++ start/passport-init.js
@@ -1,39 +1,69 @@
 var LocalStrategy = require('passport-local').Strategy;
 var bCrypt = require('bcrypt-nodejs');
 // temporary data store
-var user = {};
+var users = {};
+
 module.exports = function(passport) {
+    
+    // passport need to be able to serialize and deserialize users to support persistant login sessions
     passport.serializeUser(function(user, done) {
         console.log('serialize user:', user.username);
-        return done(null, user.username);
+        // return the unique id for the user
+        done(null, user.username);
     });
 
-    possport.deserializeUser(function(user, done) {
-        return done('we have not implemented this', false);
+    // deserialize use will call with the unique id provide by serialize
+    passport.deserializeUser(function(username, done) {
+        return done(null, users[username]);
     });
 
     passport.use('login', new LocalStrategy({
-            passReqToCallBack: true
+            passReqToCallback: true
         },
         function(req, username, password, done) {
-            return done('we have not implemented this', false);
+            console.log(users);
+            if(users[username]) {
+                console.log('User Not Found with username: ' + username);
+                return done(null, false);
+            }
+
+            if(isValidPassword(users[username], password)) {
+                return done(null, users[username]);
+            }
+            else {
+                console.log('Invalid password ' + username);
+                return done(null, false);
+            }
+            // return done('we have not implemented this', false);
         }
     ));
 
     passport.use('signup', new LocalStrategy({
-            passReqToCallBack : true
+            passReqToCallback : true // allow us to pass back the entire request to the callback
         },
         function(req, username, password, done) {
-            return done('we have not implemented this', false);
+            if(users[username]) {
+                console.log('User already exits with username: ', username);
+                return done(null, false);
+            }
+            
+            users[username] = {
+                username: username,
+                password: createHash(password)
+            }
+
+            console.log(users[username].username + ' Registration successful');
+            return done(null, users[username]);
+            //return done('we have not implemented this', false);
         }
     ));
 
-    var isValidPassword = function(user, passport) {
+    var isValidPassword = function(user, password) {
         return bCrypt.compareSync(password, user.passport);
     };
     // generate hash using bCrypt
-    var createHash = function(passport) {
-        return bCrypt.hashSync(password, bCrypt.getSaltSync(10), null);
+    var createHash = function(password) {
+        return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
     };
     
 }
